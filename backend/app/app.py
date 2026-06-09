42ba5ee88907dd89bbfc00eee431abe7dd916ccf
MalvaLess <spider3899@gmail.com>
2026-06-08 22:12:56 -0300

    Se aceptan peticiones de cualquier origen

--- backend/app/app.py
+++ backend/app/app.py
@@ -39,8 +39,7 @@ CORS(
     resources={
         r"/api/*": {
             "origins": [
-                "http://localhost:5173",
-                "http://127.0.0.1:5173"
+                "*"
             ],
             "methods": ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
             "allow_headers": ["Content-Type", "Authorization"]
